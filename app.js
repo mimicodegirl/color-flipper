0cb6e6e23935dfe1110fbd31163a87f77cb00714
Mariam Banire <86365618+mimicodegirl@users.noreply.github.com>
2021-07-18 03:55:02 -0700

    Update app.js
    added colors

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025","purple", "fuschia pink"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
